a0995b9ca4b1a090f52b88febea19710e529efae
NaufalZuhdyRabbani <naufalkzrz@gmail.com>
2025-10-22 09:00:44 +0700

    first commit

--- PAWPraktikum1_Node_20230140061/server.js
+++ PAWPraktikum1_Node_20230140061/server.js
@@ -1,14 +1,28 @@
-const http = require('http');
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = 3001;
+const morgan = require("morgan");
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// Impor router
+const presensiRoutes = require("./routes/presensi");
+const reportRoutes = require("./routes/reports");
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello dari Node.js! Naufal Zuhdy Rabbani');
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use((req, res, next) => {
+  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+  next();
 });
-
-server.listen(port, hostname, () => {
-  console.log(`Server berjalan di http://${hostname}:${port}/`);
+app.get("/", (req, res) => {
+  res.send("Home Page for API");
+});
+const ruteBuku = require("./routes/books");
+app.use("/api/books", ruteBuku);
+app.use("/api/presensi", presensiRoutes);
+app.use("/api/reports", reportRoutes);
+app.listen(PORT, () => {
+  console.log(`Express server running at http://localhost:${PORT}/`);
 });
